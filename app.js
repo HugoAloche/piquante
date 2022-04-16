b5e00a2852db88a7188b36579c91e53e7d0cf961
Hugo Aloche <haloche035@gmail.com>
2022-04-16 12:45:31 +0200

    feat: mise ├á jour pages

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log("reçu");
+    next();
+})
+
+app.use((req, res, next) => {
     res.json({ message: 'Votre requête à bien été reçu!' })
 })
 
