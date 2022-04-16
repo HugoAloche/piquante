75263580d26f28078e4dc3b2bd07ec152eb60668
Hugo Aloche <haloche035@gmail.com>
2022-04-16 13:15:34 +0200

    feat: mise ├á jour pages

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log("reçu");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 })
 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête à bien été reçu!' })
+app.use('/api/auth/login', (req, res, next) => {
+    res.header()
+    res.status(200).json(message = 'Salut');
+    next();
 })
 
 module.exports = app;
