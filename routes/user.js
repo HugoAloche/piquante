663963adef53935e7f8beee5dce9a7e0016267c5
= <=>
2022-05-13 14:41:15 +0200

    feat: modif pages

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.js');
 
-//* Si je veux me connecter
 router.post('/login', userCtrl.connectProfile);
 router.post('/signup', userCtrl.createProfile)
 
